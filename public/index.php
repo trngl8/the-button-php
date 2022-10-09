250f6145f00c700ac57e4bdf42a26e1f47df29b7
mvchn <movchan@gmail.com>
2022-10-09 23:07:48 +0300

    add routing

--- public/index.php
+++ public/index.php
@@ -2,17 +2,29 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-$message = $_POST['message'];
+$request = Request::createFromGlobals();
 
-header('Content-Type: application/json');
-
-$data = [
-    "id" => 1,
-    "timestamp" => time(),
-    "uri" => $uri,
-    "weight" => 32
+$map = [
+    '/hello' => 'hello',
+    '/bye'   => 'bye',
 ];
 
-echo json_encode($data);
+$path = $request->getPathInfo();
+
+if (isset($map[$path])) {
+    $data = [
+        "id" => 1,
+        "timestamp" => time(),
+        "uri" => $path,
+        "weight" => 32
+    ];
+
+    $response = new JsonResponse($data);
+} else {
+    $response = new JsonResponse('Not Found', 404);
+}
+
+$response->send();
