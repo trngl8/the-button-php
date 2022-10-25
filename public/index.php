8bd29a77ce5f08458474d93bff79151ba36db42e
mvchn <movchan@gmail.com>
2022-10-25 20:02:20 +0300

    clean index

--- public/index.php
+++ public/index.php
@@ -3,32 +3,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-#use Buki\Router\Router;
 
 use App\Core\Router;
 
 $request = Request::createFromGlobals();
 
-//TODO: create routing:
-//https://medium.com/the-andela-way/how-to-build-a-basic-server-side-routing-system-in-php-e52e613cf241
-
-$map = [
-    '/api' => 'DefaultController',
-    '/api/hello' => 'hello',
-];
-
 $path = $request->getPathInfo();
-
-//$router = new Router([
-//    'paths' => [
-//        'controllers' => 'Controller',
-//    ],
-//    'namespaces' => [
-//        'controllers' => 'Controller',
-//    ],
-//]);
 
 $router = new Router($request);
 
@@ -53,10 +34,5 @@ $router->get('/api', function(Request $request, Response $response) {
 
     return $response;
 });
-
-//$router->get('/default', 'DefaultController@main');
-//
-//// For auto discovering all methods and URIs
-//$router->controller('/users', 'UserController');
 
 $router->run();
